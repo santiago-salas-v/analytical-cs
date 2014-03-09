c1ed1c74d8bff897546221014694cb35de40cfe7
Santiago Salas <santiago.salas.v@hotmail.de>
2014-03-09 02:17:26 -0600

    Revert "Cross compatibility with Linux / Mac"
    This reverts commit 73f5362b62fea3aa12f4fc9efa3cfe16c56e6fc5.

--- bufferSolution.m
+++ bufferSolution.m
@@ -1,12 +1,12 @@
 %% BUFFER SOLUTIONS
 % Calcula disoluciones amortiguador 'buffer' , basado en el balance
-% de materia y equilibrios ï¿½cido - base.
+% de materia y equilibrios ácido - base.
 %% Esquema
 % 
 % <<../utils/diagrama_1.png>>
 % 
 %% Reacciones
-% _1.- Adiciï¿½n de Base Fuerte LiB y / o ï¿½cido Fuerte HAf_
+% _1.- Adición de Base Fuerte LiB y / o Ácido Fuerte HAf_
 % $\begin{array}{cccccccc|cc} 
 % (1)& & & LiB   & {\bf \longrightarrow \atop \leftarrow} &
 % Li^{(+)} & + & B^{(-)} & 
@@ -25,7 +25,7 @@
 % Kw = C_{H_3Oeq^{(+)}} \times C_{OHeq^{(-)}}  = 
 %      10^{-14} & = K_{c3} \times C_{H_2Oeq}^2 \\
 % \end{array}$
-% _2.- Adiciï¿½n de amortiguador / buffer HA + NaA_
+% _2.- Adición de amortiguador / buffer HA + NaA_
 % $\begin{array}{cccccccc|cc} 
 % (5)& & & NaA   & {\bf \longrightarrow \atop \leftarrow} &
 % Na^{(+)} & + & A^{(-)} & 
@@ -44,20 +44,20 @@
 % Kw = C_{H_3Oeq^{(+)}} \times C_{OHeq^{(-)}}  = 
 %      10^{-14} & = K_{c3} \times C_{H_2Oeq}^2 \\
 % \end{array}$
-%% Condiciones iniciales y alimentaciï¿½n
+%% Condiciones iniciales y alimentación
 % * Partir de agua pH=7 al inicio (t0). 
 % $C_{H_3O^{(+)}(t0)}=10^{-7}gmol/L$
-% * Agregar base fuerte $V_0 C_{LiB_0}$ y ï¿½cido fuerte 
+% * Agregar base fuerte $V_0 C_{LiB_0}$ y ácido fuerte 
 %   $V_0 C_{HAf_0}$.
-% * Agregar amortiguador en forma de ï¿½cido $V_0 C_{HA_0}$ + sal 
+% * Agregar amortiguador en forma de ácido $V_0 C_{HA_0}$ + sal 
 %   $V_0 C_{NaA_0}$.
 % La sal se disocia
-% completamente, el ï¿½cido de acuerdo con el equilibrio ï¿½cido-base.
-% * Parï¿½metros:
+% completamente, el ácido de acuerdo con el equilibrio ácido-base.
+% * Parámetros:
 %
 % $Vr$
 %
-% _Alimentaciï¿½n_
+% _Alimentación_
 %
 % $V_0 C_{HA_0}$
 %
@@ -83,7 +83,7 @@
 %
 %% Ecuaciones
 %
-% * Balance de materia para avance $\xi_j$ de reacciï¿½n j en 'batch':
+% * Balance de materia para avance $\xi_j$ de reacción j en 'batch':
 % $(C_{i} - C_{i0}) = \Sigma_j (\nu_{ij} \times \xi_j)$
 %
 % $\begin{array}{ccccccc}
@@ -127,7 +127,7 @@
 % (+1) \times \xi_6 & + (-1) \times \xi_7  \\
 % \end{array}$
 %
-% Eliminar los avances de reacciï¿½n $\xi_i$ a favor de los parï¿½metros 
+% Eliminar los avances de reacción $\xi_i$ a favor de los parámetros 
 % especificados, con la sig. diferencia: 
 %
 % $\begin{array}{ccc}
@@ -160,7 +160,7 @@
 %       C_{OH^{(-)}}
 % \end{array}$
 %
-% Reordenando se observa que en este caso no es una ecuaciï¿½n 
+% Reordenando se observa que en este caso no es una ecuación 
 % independiente del balance de materia.
 %
 % $\begin{array}{ccc}
@@ -177,7 +177,7 @@
 %
 % $Kw = C_{H_3Oeq^{(+)}} \times C_{OHeq^{(-)}}$
 %
-% Resulta un sistema algebrï¿½ico no lineal, 4 ecuaciones 
+% Resulta un sistema algebráico no lineal, 4 ecuaciones 
 % *independientes* y 4 variables.
 % $\begin{array}{cccccccccc} 
 % {V_0 \over Vr} \times \left({C_{HA_0}} + \left( C_{HAf_0}
@@ -195,8 +195,8 @@
 % + & C_{H_3Oeq^{(+)}} & \times & 
 % C_{OHeq^{(-)}} \\
 % \end{array}$
-%% Soluciï¿½n
-% Reducciï¿½n a 1 polinomio de 3er grado.
+%% Solución
+% Reducción a 1 polinomio de 3er grado.
 % 
 % $\begin{array}{cccccc} 
 % 0 & = & + & 
@@ -214,8 +214,8 @@
 % \left[ {C_{H_3Oeq^{(+)}} \over Ka} \right]^0 \\
 % \end{array}$
 %
-% * Fijar alimentaciï¿½n.
-% * Resolver ecuaciï¿½n cï¿½bica para 
+% * Fijar alimentación.
+% * Resolver ecuación cúbica para 
 % $C_{H_3Oeq^{(+)}}$ , calcular pH.
 % * Calcular $C_{OHeq^{(-)}}$ por ec. 4.
 % * Calcular $C_{Aeq^{(-)}}$ y $C_{HAeq}$ por ecs. 1,2.
@@ -235,7 +235,7 @@
 %
 % $C_{Af^{(-)}}$
 %% Capacidad del amortiguador $\beta$
-% Definiciï¿½n
+% Definición
 %
 % $\beta = - {dC_{HAf(t0)} \over dpH}
 % = +{dC_{LiB(t0)} \over dpH}$
@@ -312,10 +312,6 @@
 % \right]$
 %% 
 function bufferSolution()
-% Compatibilidad cruzada
-if ~ispc
-    feature('DefaultCharacterSet','windows-1252');
-end
 % Generar ventana
 screenSize  = get(0,'MonitorPositions');
 Ventana     = findobj('Tag','BufferSolutionGui');
@@ -350,7 +346,7 @@ if isempty(Ventana) || ~ishandle(Ventana) || ~isscalar(Ventana)
     texto1  = uicontrol('Parent',Panel2,...
         'Units','normalized',...
         'Style','text',...
-        'String','PARï¿½METROS',...
+        'String','PARÁMETROS',...
         'BackgroundColor','white',...
         'Position',[30/100/2-1/6/2,1-1/8+1/4/6,1/6,1/4/6]);
     texto2  = uicontrol('Parent',Panel2,...
@@ -451,7 +447,7 @@ if isempty(Ventana) || ~ishandle(Ventana) || ~isscalar(Ventana)
     BotonDeReporte  = uipushtool(Herramientas,...
         'CData',            pageicon,...        
         'Tag',              'uipushtool1',...
-        'TooltipString',    'Ver procedimiento de soluciï¿½n',...
+        'TooltipString',    'Ver procedimiento de solución',...
         'Separator',        'on');
     Eje1            = axes('Parent',Panel2,...
         'Units','normalized',...
@@ -596,7 +592,7 @@ elseif discriminante == 0
     % (X-x1_R)^2(X-x3_R) = 0
     raizSeleccionada   = find( sonPosit );
 elseif discriminante < 0
-    % 2 raices complejas conjugadas y 1 raï¿½z real
+    % 2 raices complejas conjugadas y 1 raíz real
     % (X-x1_C)(X-x1_C_conj)(X-x3_R) = 0
     raizSeleccionada   = find( sonreales );
 end
@@ -769,7 +765,7 @@ try
         % Poner estos valores en la tabla uitable1 (a la izq.)}
         set(handles.Tabla_Entrada,'Data',Datos); 
         %
-        % Correr el cï¿½digo para actualizar ( o generar en dado caso) la grï¿½fica
+        % Correr el código para actualizar ( o generar en dado caso) la gráfica
         % solicitada.
         funcionDeCalcular(hObject,eventData,handles);
     end
